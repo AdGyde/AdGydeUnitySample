d2d6434122886b4f52547b727c0d2c423a4fabea
AdGyde <adgydesolutions@gmail.com>
2020-01-20 16:17:35 +0530

    AdGyde Unity SDK v4.1.2.1 - Sample App
    AdGyde Unity SDK v4.1.2.1 - Sample App

---
+++ AdGyde Unity Android Sample App/Assets/Scripts/EventClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventClass : System.Object
+{
+	public string name = "";
+	public string date = "";
+	public string time = "";
+	//public JSONArray parameter = new JSONArray();
+	public string start = "";
+	public string end = "";
+	public int duration = 0;
+
+}
---
+++ AdGyde Unity Android Sample App/Assets/Scripts/UserDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDetails
+{
+	public string user = "";
+	public string token = "";
+	public string appkey = "";
+
+
+}
